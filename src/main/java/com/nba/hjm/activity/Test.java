c1d69e862a8035be8bbf4d0e6be3e8663eeefc3d
Bsman <2287659768@qq.com>
2016-08-03 11:02:18 +0800

    添加一个Test文件

--- src/main/java/com/nba/hjm/activity/Test.java
+++ src/main/java/com/nba/hjm/activity/Test.java
@@ -1,7 +1,15 @@
 package com.nba.hjm.activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Administrator on 2016/8/2.
  */
-public class Test {
+public class Test extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
 }
